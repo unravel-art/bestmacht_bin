cc501bf0c56bf3c356acdc9c0d748da5d33e4fbd
unravel-art <13424343855@163.com>
2019-11-08 23:14:43 +0800

    [issueNo]:#1 [creator]:unravel-art [description]:demo首提交测试

---
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/galaxy/user")
+public class UserController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
+    @RequestMapping(value = "/getUserName")
+    public String getUserName(@RequestParam(value = "userId") String userId) {
+        return  StringUti== null ? "null" : userId;
+    }
+}
